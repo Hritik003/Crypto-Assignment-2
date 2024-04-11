4faa8f5e8edbfbfcd6a6e3f1f281028ca4f342cd
Hritik <hritikraj_03@outlook.com>
2024-04-11 11:10:40 +0530

    created 2 routes and running

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, jsonify,redirect, request, send_file, url_for
 import json
+# from django.http import response
 from flask import jsonify  # For JSON responses
 from blockchain import SupplyChainBlockchain
-import uuid
-import random
 
+#intitializing flask
 app = Flask(__name__)
 blockchain = SupplyChainBlockchain()
 
-@app.route("/add/user", methods=['POST'])
-def add_user():
+# @app.route("/add/user", methods=['POST'])
+# def add_user():
     
     
 
-@app.route("/users")
+@app.route("/users", methods = ['GET'])
 def showUsers():
-    users = SupplyChainBlockchain.users
+    users = blockchain.nodes
     response={
         'users':users
     }
@@ -23,28 +23,40 @@ def showUsers():
     return jsonify(response),200
 
 
-@app.route("/add/transaction", methods=['POST'])
-def add_transaction():
+# @app.route("/add/transaction", methods=['POST'])
+# def add_transaction():
 
-@app.route("/transactions", methods=['GET'])
-def list_transactions():
+# @app.route("/transactions", methods=['GET'])
+# def list_transactions():
 
-@app.route("/mine", methods=['GET'])
-def mine_block():
+# @app.route("/mine", methods=['GET'])
+# def mine_block():
 
 @app.route("/chain", methods=['GET'])
 def get_chain():
+    response={
+        'chain':blockchain.chain,
+        'length':len(blockchain.chain)
+    }
+    return jsonify(response),200
 
-@app.route("/register/node", methods=['POST'])
-def register_node():
+# @app.route("/register/node", methods=['POST'])
+# def register_node():
 
-@app.route("/resolve", methods=['GET'])
-def resolve_conflicts():
+# @app.route("/resolve", methods=['GET'])
+# def resolve_conflicts():
 
-@app.route("/product/<product_id>", methods=['GET'])
-def view_product_history(product_id):
+# @app.route("/product/<product_id>", methods=['GET'])
+# def view_product_history(product_id):
 
+if __name__ == '__main__':
+    from argparse import ArgumentParser
 
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5001, type=int, help='Listening on port')
+    args = parser.parse_args()
+    port = args.port
+    app.run(host = '0.0.0.0', port = port, debug=True)
 
 
 
